919a2f499f4d377e36afc0f178454a6a4966031e
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-07-27 16:06:50 +0100

    Added JSDoc comments

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,19 @@
 // TODO (Ex.0): document and write tests for this function
-
+/**
+ * Given a number, returns a negative version of the number
+ * @param {number} n 
+ * @returns {number} 'n' as a negative
+ */
 function asNegative(n) {
-  // ...
+  if (n > 0) {
+    return -n
+  } else {
+    return n
+  }
 }
+
+
+console.log(asNegative(7))
+
 
 
